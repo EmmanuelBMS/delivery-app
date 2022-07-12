9cc1ba664376da61ca51a6d3f63fa8b236b325b9
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-07-12 19:20:16 -0300

    feat(api/routes/index): Adiciona rota /products

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -1,8 +1,11 @@
-const loginRoute = require('./login.route')
-const userRoute = require('./user.route')
+const loginRoute = require('./login.route');
+const userRoute = require('./user.route');
+const productRoute = require('./product.route');
+
 const appRouter = (app) => {
   app.use('/login', loginRoute);
   app.use('/register', userRoute);
-}
+  app.use('/products', productRoute);
+};
 
 module.exports = appRouter;
