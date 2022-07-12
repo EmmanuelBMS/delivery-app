717b2ebc8c6bdb285cc145e5f4e6206b9fc2bc1d
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-07-11 22:59:43 -0300

    feat(backend/src/api/service): create service user

---
+++ back-end/src/api/services/user.service.js
@@ -0,0 +1,31 @@
+const crypto = require('crypto');
+const { User } = require("../../database/models");
+const user = require("../../database/models/user");
+const generateError = require("../../helpers/generateErro");
+const { generate } = require("../../helpers/tokenAuth");
+
+const findOne =  async (email) => {
+  console.log(email);
+  const user = await User.findOne({ where: { email } });
+  if(user) {
+    throw generateError({ status: 404, message: 'Email already exist'});
+  }
+  return user;
+}
+
+const create = async (newUser) => {
+  await findOne(newUser.email);
+  const passCrypto = crypto.createHash('md5').update(newUser.password).digest("hex");
+  newUser.password = passCrypto
+  const { dataValues } =  await User.create(newUser)
+  const userCreated = dataValues
+  delete userCreated.password
+  const token = generate(userCreated)
+  return { token }
+}
+
+
+module.exports = {
+  create,
+  findOne,
+};
