3009b225a31489ea486505a12f882ab2a72dc4da
FAY <brunofay1@hotmail.com>
2022-07-13 01:24:59 -0300

    refactor(app):rota de products criada

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/login/Login';
+import Products from './pages/product/Products';
 import Register from './pages/register/Register';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Navigate to="/login" replace /> } />
-      <Route path="/login" element={ <Login /> } />
-      <Route path="/register" element={ <Register /> } />
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/customer" >
+        <Route path="products" element={<Products />} />
+      </Route>
     </Routes>
   );
 }
