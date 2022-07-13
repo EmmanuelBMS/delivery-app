2ba763875922070426de08cda4fbccb8c598ea06
FAY <brunofay1@hotmail.com>
2022-07-13 14:36:41 -0300

    feat(pages/product):finalizado a parte visual da pagina de produtos

--- front-end/src/pages/product/Products.jsx
+++ front-end/src/pages/product/Products.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import { Plus, Minus } from 'phosphor-react'
+import './products.css'
 
 const api = [{
   id: 1,
@@ -14,19 +16,62 @@ const api = [{
 }]
 
 export default function Products() {
+  const changeDotToCommaOfPrice = (price) => price.toString().replace('.', ',')
   return (
     <div>
       <Navbar />
-      <main>
+      <main className='flex py-6 gap-6 px-14'>
         {
           api.map((item) => (
-            <div>
-              <span></span>
-              <img />
-              <div>
-                <button></button>
-                { }
-                <button></button>
+            <div
+              className="productCard"
+              key={item.id}>
+              <span
+                data-testid={`customer_products__element-card-price-${item.id}`}
+                className='bg-blue-200 py-1 px-2 rounded relative left-2 top-1 text-xl font-extrabold'
+              >
+                R$ {changeDotToCommaOfPrice(item.price)}
+              </span>
+              <img
+                data-testid={`customer_products__element-card-price-${item.id}`}
+                src={item.urlPrice}
+              />
+              <div className='flex flex-col bg-blue-100 py-2 items-center'>
+                <span
+                  data-testid={`customer_products__element-card-title-${item.id}`}
+                >
+                  {item.name}
+                </span>
+                <div className='flex rounded border bg-[#256B52] px-2 mb-3'>
+                  <button
+                    data-testid={`customer_products__button-card-rm-item-${item.id}`}
+                    className='flex justify-center items-center'
+                    type='button'
+                  >
+                    <Minus
+                      className='mr-2'
+                      size={18}
+                      color={"white"}
+                    />
+                  </button>
+
+                  <span
+                    data-testid={`customer_products__input-card-quantity-${item.id}`}
+                    className=' bg-white text-zinc-600 px-3'>
+                    {1}
+                  </span>
+                  <button
+                    data-testid={`customer_products__button-card-add-item-${item.id}`}
+                    className='flex justify-center items-center'
+                    type='button'
+                  >
+                    <Plus
+                      className='ml-2'
+                      size={20}
+                      color={"white"}
+                    />
+                  </button>
+                </div>
               </div>
             </div>
           ))
