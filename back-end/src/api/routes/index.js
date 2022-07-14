797998b1645511f8021e17e1a11b2aba827dbd30
morallys <morallys.juan@gmail.com>
2022-07-14 17:25:31 -0300

    feat(api/routes): Cria rota /sales

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -1,11 +1,13 @@
 const loginRoute = require('./login.route');
 const userRoute = require('./user.route');
 const productRoute = require('./product.route');
+const saleRoute = require('./sale.route');
 
 const appRouter = (app) => {
   app.use('/login', loginRoute);
   app.use('/register', userRoute);
   app.use('/products', productRoute);
+  app.use('/sales', saleRoute);
 };
 
 module.exports = appRouter;
