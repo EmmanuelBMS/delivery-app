bd24664bfc933b48d4326f8c49566de1d2b411de
morallys <morallys.juan@gmail.com>
2022-07-14 17:19:34 -0300

    feat(database/models/user): Insere relacionamento com Sale

--- back-end/src/database/models/user.js
+++ back-end/src/database/models/user.js
@@ -10,9 +10,9 @@ module.exports = (sequelize, DataTypes) => {
     password: DataTypes.STRING,
     role: DataTypes.STRING,
   }, { timestamps: false, tableName: 'users' });
-  // User.associate = (models) => {
-  //   User.hasMany(models.,
-  //     { foreignKey: 'userId', as: 'blogPost' });
-  // };
+  User.associate = (models) => {
+    User.hasMany(models.Sale,
+      { foreignKey: 'user_id', as: 'Sales' });
+  };
   return User;
 };
