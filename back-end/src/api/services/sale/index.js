952d25a0d61f1aa16449873ee5a9bafd97bbfaab
morallys <morallys.juan@gmail.com>
2022-07-19 18:31:49 -0300

    feat(api/services/sale): Insere funções para chamada aos models
    - Cria transactions na create, visando atomicidade na transações ao banco de dados na realização dos relacionamentos

--- back-end/src/api/services/sale/index.js
+++ back-end/src/api/services/sale/index.js
@@ -1,12 +1,49 @@
-const { Sale } = require('../../../database/models');
+const { Sale, SaleProduct, sequelize, Product } = require('../../../database/models');
 
-const findById = async (userId) => {
-  const sales = Sale.findAll({ where: { userId } });
+const create = async (newSale) => {
+  const { sale, product } = newSale;
+
+  await sequelize.transaction(async (t) => {
+    const { dataValues } = await Sale.create(sale, { transaction: t });
+
+    const saleProducts = product.map((prod) => {
+      const { productId, quantity } = prod;
+      const saleProduct = { saleId: dataValues.id, productId, quantity };
+
+      return SaleProduct.create(saleProduct, { transaction: t });
+    });
+
+    await Promise.all(saleProducts);
+  });
+};
+
+const findByIdSale = async (id) => {
+  const sales = Sale.findAll({
+    where: { id },
+    include: [
+      { model: Product, as: 'products' },
+    ],
+  });
+
   return sales;
 };
 
-const create = async (newSale) => {
-  await Sale.create(newSale);
+const findAllByIdSales = async (id, role) => {
+  if (role === 'customer') {
+    return Sale.findAll({
+      where: { userId: id },
+      include: [
+        { model: SaleProduct, as: 'products' },
+      ],
+    });
+  }
+
+  return Sale.findAll({
+    where: { saleId: id },
+    include: [
+      { model: SaleProduct, as: 'products' },
+    ],
+  });
 };
 
-module.exports = { create, findById };
+module.exports = { create, findByIdSale, findAllByIdSales };
